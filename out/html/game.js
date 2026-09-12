e0935bb2b69dc6adc176782f846bdb8085d48f55
OrangeyAutumn <woodjoshua20@gmail.com>
2026-09-12 20:59:31 +0100

    Refactor modifyUI to main and streamline updateSidebar

--- out/html/game.js
+++ out/html/game.js
@@ -9,12 +9,12 @@
                  month: 'short',
                  day: 'numeric' };
 
-var modifyUI = function(dendryUI) {
+  var main = function(dendryUI) {
     ui = dendryUI;
     game = ui.game;
 
     // Add your custom code here.
-};
+  };
 
   var TITLE = "Social Democracy: An Alternate History" + '_' + "Autumn Chen";
 
@@ -205,54 +205,32 @@ var modifyUI = function(dendryUI) {
   };
 
   // TODO: have some code for tabbed sidebar browsing.
- window.updateSidebar = function() {
-       $('#qualities').empty();
-       var scene = dendryUI.game.scenes[window.statusTab];
-       if (!scene) {
-           console.warn('updateSidebar: scene "' + window.statusTab + '" not found, falling back to "status"');
-           window.statusTab = "status";
-           scene = dendryUI.game.scenes[window.statusTab] || Object.values(dendryUI.game.scenes)[0];
-       }
-       if (scene && scene.onArrival) {
-           try {
-               dendryUI.dendryEngine._runActions(scene.onArrival);
-           } catch (e) {
-               console.error('Error running onArrival actions for sidebar scene:', e);
-           }
-       }
-       var displayContent = [];
-       if (scene && scene.content) {
-           try {
-               displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
-           } catch (e) {
-               console.error('Error making display content for sidebar scene:', e);
-           }
-       }
-       $('#qualities').append(dendryUI.contentToHTML.convert(displayContent));
-   };
+  window.updateSidebar = function() {
+      $('#qualities').empty();
+      var scene = dendryUI.game.scenes[window.statusTab];
+      dendryUI.dendryEngine._runActions(scene.onArrival);
+      var displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
+      $('#qualities').append(dendryUI.contentToHTML.convert(displayContent));
+  };
 
+  window.changeTab = function(newTab, tabId) {
+      if (tabId == 'poll_tab' && dendryUI.dendryEngine.state.qualities.historical_mode) {
+          window.alert('Polls are not available in historical mode.');
+          return;
+      }
+      var tabButton = document.getElementById(tabId);
+      var tabButtons = document.getElementsByClassName('tab_button');
+      for (i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].className = tabButtons[i].className.replace(' active', '');
+      }
+      tabButton.className += ' active';
+      window.statusTab = newTab;
+      window.updateSidebar();
+  };
 
-window.changeTab = function(newTab, tabId) {
-       if (tabId == 'poll_tab' && dendryUI.dendryEngine.state.qualities.historical_mode) {
-           window.alert('Polls are not available in historical mode.');
-           return;
-       }
-       var tabButton = document.getElementById(tabId);
-       var tabButtons = document.getElementsByClassName('tab_button');
-       // clear active class if any
-       for (var i = 0; i < tabButtons.length; i++) { 
-         tabButtons[i].className = tabButtons[i].className.replace(' active', ''); 
-       }
-       // only add active class if the element exists
-       if (tabButton) {
-         tabButton.className += ' active';
-       } else {
-         console.warn('changeTab: tab button with id "' + tabId + '" not found.');
-       }
-       window.statusTab = newTab;
-       window.updateSidebar();
-   };
-
+  window.onDisplayContent = function() {
+      window.updateSidebar();
+  };
 
   /*
    * This function copied from the code for Infinite Space Battle Simulator
@@ -288,7 +266,7 @@ window.changeTab = function(newTab, tabId) {
 
   window.justLoaded = true;
   window.statusTab = "status";
-  window.dendryModifyUI = modifyUI;
+  window.dendryModifyUI = main;
   console.log("Modifying stats: see dendryUI.dendryEngine.state.qualities");
 
   window.increaseFontSize = function() {
